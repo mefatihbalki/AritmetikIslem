eec41d16fe879c432487df1387017e909f388db6
Mehmet Fatih Balkı <159441317+mefatihbalki@users.noreply.github.com>
2024-02-20 17:33:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+
+        //Kullanıcıdan Alınan Değerler
+        Scanner inp = new Scanner(System.in);
+        System.out.print("a değerini giriniz: ");
+        a = inp.nextInt();
+
+        System.out.print("b değerini giriniz: ");
+        b = inp.nextInt();
+
+        System.out.print("c değerini giriniz: ");
+        c = inp.nextInt();
+
+        int sonuc = a + b * c - b;
+                System.out.print("işlem Sonucu: " + sonuc);
+
+    }
+}
